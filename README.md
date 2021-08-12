996d1f520e0aabd30c9fda9257c9330356b5b1d5
Alejandro <dlaparicio87@gmail.com>
2021-08-12 19:20:06 -0400

    Uploading files with curl

--- README.md
+++ README.md
@@ -15,11 +15,11 @@ Projects are created with:
 * Python 3
 
 ## Setup
- 
-1. Create a virtual enviroment with python ```virtualenv -p python3 prove```
-2. Then ```cd prove```
-3. Execute ```soure bin/activate``` to activate virtualenv
-4. Create an ```src``` folder and the ```cd src```
-5. Clone the repo ```https://github.com/aaparicio87/proveyourworth.git``` and ```cd proveyourworth```
-6. Execute ```pip3 install -r requirements.txt```
-7. Run the script ```python test.py```  
+1. You need to install in linux ```libcurl4-gnutls-dev librtmp-dev``` to use pycurl  
+2. Create a virtual enviroment with python ```virtualenv -p python3 prove```
+3. Then ```cd prove```
+4. Execute ```soure bin/activate``` to activate virtualenv
+5. Create an ```src``` folder and the ```cd src```
+6. Clone the repo ```https://github.com/aaparicio87/proveyourworth.git``` and ```cd proveyourworth```
+7. Execute ```pip3 install -r requirements.txt```
+8. Run the script ```python test.py```  
--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import requests 
 from bs4 import BeautifulSoup 
 from PIL import Image, ImageDraw
-from pathlib import Path
+import pycurl
+from urllib.parse import urlencode
 
 start_url = "http://www.proveyourworth.net/level3/start"
 activate_url = "http://www.proveyourworth.net/level3/activate?statefulhash"
 payload_url = "http://www.proveyourworth.net/level3/payload"
-file_path = Path("./")
 
 session = requests.Session()
 
@@ -33,26 +33,41 @@ def write_image(image):
     draw.text((20,70), "Alejandro Aparicio Guerra, \n Hash:{} \n dlaparicio87@gmail.com \n Backend Developer".format(get_hash(start_url)), fill=(50,168,82))
     image.save("image.jpg","JPEG")
 
-def upload(payload):
+def upload_pycurl(payload):
     pay = session.get(payload)
-    post_uri = "{}".format(pay.headers['X-Post-Back-To'])
-    print(post_uri)
-
-    file = {
-        "code":open(file_path / "test.py","rb"),
-        "resume":open(file_path / "cv.pdf","rb"),
-        "image":open(file_path / "image.jpg","rb")
-    }
+    post_url = "{}".format(pay.headers['X-Post-Back-To'])
 
     data = {
         "email":"dlaparicio87@gmail.com",
         "name":"Alejandro Aparicio Guerra",
         "aboutme": "I'm backend developer in Python, Javascript and PHP and with knowledges of fronted libraries as React.js."
     }
-
-    request = session.post(post_uri, data=data, files=file)
-    print(request.status_code)    
     
+    c = pycurl.Curl()
+    c.setopt(c.URL, post_url)
+
+    # Form data must be provided already urlencoded.
+    postfields = urlencode(data)
+    # Sets request method to POST,
+    # Content-Type header to application/x-www-form-urlencoded
+    # and data to send in request body.
+    c.setopt(c.POSTFIELDS, postfields)
+
+    c.setopt(c.UPLOAD, 1)
+    code = open("test.py","rb")
+    resume = open( "cv.pdf","rb")
+    image = open("image.jpg","rb")
+
+    c.setopt(c.READDATA, code)
+    c.setopt(c.READDATA, resume)
+    c.setopt(c.READDATA, image)
+
+    c.perform()
+    c.close()
+    code.close()
+    resume.close()
+    image.close()
+
 
 if __name__ == '__main__':
     print('Start Session')
@@ -65,6 +80,6 @@ if __name__ == '__main__':
     write_image(get_image_to_sign(payload_url))
     print('End Write Image')
     print('Start Upload Files')
-    upload(payload_url)
+    upload_pycurl(payload_url)
     print('End Upload Files')
     
